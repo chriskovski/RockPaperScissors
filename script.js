9330279e8b12c6d31d455bce9bbb7d0eeea33d10
Chris <christophschmid96@yahoo.de>
2022-03-03 16:54:17 +0100

    You can now choose Rock, Paper or Scissors (case-insensitive) + play a round against the computer. Console will tell if you won, lost or tied.

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-console.log("Hello World");
+let choice = ['Rock', 'Paper', 'Scissors'];
+
+function computerPlay(x) {
+    x = choice[Math.floor(Math.random()*choice.length)];
+    return x;
+}
+
+
+function playRound() { 
+    if (playerSelect == choice[0] && computerSelect == choice[1]) {
+        console.log("You Lose! Paper beats Rock!");
+    }else if (playerSelect == choice[0] && computerSelect == choice[2]) {
+        console.log("You Win! Rock beats Scissors!");
+    }else if (playerSelect == choice[1] && computerSelect == choice[0]) {
+        console.log("You Win! Paper beats Rock!");
+    }else if(playerSelect == choice[1] && computerSelect == choice[2]) {
+        console.log("You Lose! Scissors beats Paper!");
+    }else if (playerSelect == choice[2] && computerSelect == choice[0]) {
+        console.log("You Lose! Rock beats Scissors!");
+    }else if (playerSelect == choice[2] && computerSelect == choice[1]) {
+        console.log("You Win! Scissors beats Paper!");
+    }else{
+        console.log("It's a tie!")
+    }  
+}
+
+let playerPrompt = prompt("What's your choice? (Rock, Paper, Scissors)").toLowerCase();
+let playerSelect = playerPrompt.charAt(0).toUpperCase() + playerPrompt.slice(1);
+const computerSelect = computerPlay();
